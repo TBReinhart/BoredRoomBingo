464d53167f29ddceaf820b7cb606830956a17237
Thomas Reinhart <treinha3@jhu.edu>
2014-10-26 15:41:38 -0400

    all files

---
+++ BoredRoomBingo/Errors/ErrorMessage.h
@@ -0,0 +1,13 @@
+//
+//  ErrorMessage.h
+//  BoredRoomBingo
+//
+//  Created by Tom Reinhart on 10/21/14.
+//  Copyright (c) 2014 Tom Reinhart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ErrorMessage : UIViewController
+-(void)errorMessages:(NSError*)error;
+@end
